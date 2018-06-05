48574af42b3d3152d3ee8360642ddabbd577bc4f
Jevean Pingali <jeevan.pingali@gmail.com>
2018-06-05 21:15:44 +0530

    Implemented Dropwizard Metrics

--- CompletableFuture/src/main/java/pingali/jeevan/completable_future/CompletableFutureApp.java
+++ CompletableFuture/src/main/java/pingali/jeevan/completable_future/CompletableFutureApp.java
@@ -1,28 +1,50 @@
 package pingali.jeevan.completable_future;
 
+import com.codahale.metrics.ConsoleReporter;
+import com.codahale.metrics.Meter;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
+
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class CompletableFutureApp {
     private static ExecutorService firstPool;
     private static ExecutorService secondPool;
     private static ExecutorService thirdPool;
+    private static final MetricRegistry metrics = new MetricRegistry();
+    private static final Timer responses = metrics.timer(CompletableFutureApp.class.getName());
 
     public static void main(String args[]) {
 
         createPools();
 
-        BlockingAction blockingAction = new BlockingAction();
+        ConsoleReporter reporter = ConsoleReporter.forRegistry(metrics)
+                .convertRatesTo(TimeUnit.SECONDS)
+                .convertDurationsTo(TimeUnit.MILLISECONDS)
+                .build();
+        reporter.start(1, TimeUnit.SECONDS);
 
-        UseBlockingActionResult useBlockingActionResult = new UseBlockingActionResult();
+        for(int i = 0; i < 100; i++) {
+            final Timer.Context context = responses.time();
+            try {
+                BlockingAction blockingAction = new BlockingAction();
 
-        CompletableFuture<String> step1 = CompletableFuture.supplyAsync(blockingAction::blockingActionMethod, firstPool);
-        CompletableFuture<Void> step2 = step1.thenAcceptAsync(useBlockingActionResult::use, secondPool);
-        step2.thenRunAsync(() -> System.out.println("All steps completed..."), thirdPool);
+                UseBlockingActionResult useBlockingActionResult = new UseBlockingActionResult();
+
+                CompletableFuture<String> step1 = CompletableFuture.supplyAsync(blockingAction::blockingActionMethod, firstPool);
+                CompletableFuture<Void> step2 = step1.thenAcceptAsync(useBlockingActionResult::use, secondPool);
+                step2.thenRunAsync(() -> System.out.println("All steps completed..."), thirdPool);
+            } finally {
+                context.stop();
+            }
+        }
+
 
         try {
-            Thread.sleep(200);
+            Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
