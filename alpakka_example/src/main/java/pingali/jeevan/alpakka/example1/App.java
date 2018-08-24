ce766f16f49d3e12f1ac8de60ca2556ed44a1ce5
Jeevan Pingali <jeevan.pingali@gmail.com>
2018-08-24 18:41:37 +0530

    All compilation issues fixed.

--- alpakka_example/src/main/java/pingali/jeevan/alpakka/example1/App.java
+++ alpakka_example/src/main/java/pingali/jeevan/alpakka/example1/App.java
@@ -1,14 +1,36 @@
 package pingali.jeevan.alpakka.example1;
 
+import akka.Done;
+import akka.NotUsed;
+import akka.actor.ActorSystem;
+import akka.stream.ActorMaterializer;
+import akka.stream.Materializer;
+import akka.stream.javadsl.Source;
+import java.io.File;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.TimeUnit;
+import scala.concurrent.duration.FiniteDuration;
+
 public class App {
     public static void main(String args[]) {
         final FileSystem fs = FileSystems.getDefault();
         final FiniteDuration pollingInterval = FiniteDuration.create(250, TimeUnit.MILLISECONDS);
         final int maxLineSize = 8192;
 
+        File file = new File(".");
+        
+        String path = file.getAbsolutePath() + "\\src\\main\\java\\pingali\\jeevan\\alpakka\\example1\\App.java";
+        
         final Source<String, NotUsed> lines =
                 akka.stream.alpakka.file.javadsl.FileTailSource.createLines(fs.getPath(path), maxLineSize, pollingInterval);
 
-        lines.runForeach((line) -> System.out.println(line), materializer);
+        final ActorSystem system = ActorSystem.create();
+        final Materializer materializer = ActorMaterializer.create(system);
+
+        final CompletionStage<Done> done = lines.runForeach((line) -> System.out.println(line), materializer);
+        
+        done.thenRun(() -> system.terminate());
     }
 }
